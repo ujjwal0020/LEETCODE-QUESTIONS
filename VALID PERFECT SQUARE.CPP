fbff0b5de5ac660e830519b0a9e0a12ae0a1fe94
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2023-07-11 23:48:56 +0530

    Create VALID PERFECT SQUARE.CPP

---
+++ VALID PERFECT SQUARE.CPP
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(long i=1;;i++)
+        {
+            if(i*i==num)
+                return true;
+            if(i*i>num)
+                return false;
+        }
+    }
+};
